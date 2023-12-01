9ddd9c3a59a930489e4625ec12753b40ccf85c56
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2023-12-01 11:39:07 +0100

    Day 1

---
+++ 2023/day1/day.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+fs.readFile('input.txt', 'utf8', (err, data) => {
+    if(err) {
+        console.error('erreur :', err);
+        return;
+    }
+
+    const lignes = data.split('\n');
+    const resultats = [];
+    let final = 0;
+
+    for (const ligne of lignes) {
+        if(ligne.trim() !=='') {
+            const chiffres = ligne.match(/\d/g);
+            
+            if (chiffres.length >= 2){
+                const premierChiffre = chiffres[0];
+                const dernierChiffre = chiffres[chiffres.length - 1];
+                resultats.push(premierChiffre + dernierChiffre);
+            } else {
+                resultats.push(chiffres + chiffres);
+            }
+        }
+    }
+    for (const result of resultats) {
+        final = final + parseInt(result);
+    }
+    console.log(final);
+});
+
